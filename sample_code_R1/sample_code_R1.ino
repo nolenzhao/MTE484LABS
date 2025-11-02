60b5d02c97afb57b88dd08bed9dadb1ea553b8d9
n37zhao <n37zhao@uwaterloo.ca>
2025-11-02 16:54:59 -0500

    lab2 init

--- sample_code_R1/sample_code_R1.ino
+++ sample_code_R1/sample_code_R1.ino
@@ -5,7 +5,14 @@
 int MOT_PIN = A0;   // motor angle sensor
 int BAL_PIN = A1;   // ball position sensor
 
-
+static constexpr int num_size = 4;
+static constexpr int denom_size = 5;
+// double numerator[num_size] = {5.220991266182068, 2.19347716081792, 3.01262904487014, -1.945180614295883}; 
+// double denominator[denom_size] = {1.0f, 0.4237, 0.446101301973525, 0.260556510434016, 0.078940914984297};
+float numerator[num_size] = {5.221, 2.1935, 3.0126, -1.9452}; 
+float denominator[denom_size] = {1.0f, 0.4237, 0.4461, 0.2606, 0.0789};
+float errors[num_size] = {0, 0, 0, 0};
+float cont_output[denom_size - 1] = {0, 0, 0, 0};
 // ================== Setup ==================
 void setup() {
 
@@ -18,6 +25,8 @@ void setup() {
   setMotorVoltage(0.0f);
 
   Serial.println("geeWhiz Started");
+
+
 }
 
 // ================== Loop ==================
@@ -26,39 +35,82 @@ static constexpr float Kp = 16.0f;
 static constexpr float pi = 3.141592;
 static constexpr float m = - pi/ 228;
 static constexpr float x = 7.03;
-static constexpr float target = 2;
+static constexpr float target = 0;
 static constexpr float cw_stiction = 0.26;
 static constexpr float ccw_stiction = -0.21;
 
+
+
 void  loop() {
+
+  // Proportional Controller
+  // float u_z = Kp * err; 
 
   float clamped_target = target;
   if (clamped_target > 0.7) {
     clamped_target = 0.7;
   }
 
+  // Calculate motor_encoder value, and corresponding motor position
   float motor_val = analogRead(MOT_PIN);
   float motor_pos = m * motor_val + x; 
-  float err = motor_pos - clamped_target;
-  // Proportional Controller
-  float u_z = Kp * err; 
 
-  if(u_z > 0){
-    setMotorVoltage(u_z + cw_stiction);
+  // Calculate Error 
+  float err = motor_pos - clamped_target;
+
+  // Move errors back 
+  for(int i = num_size - 1; i > 0; i--){
+    errors[i] = errors[i-1];
+  }
+  // Update front of errors array 
+  errors[0] = err;
+
+  for(int i = denom_size -1; i > 0; i--){
+    cont_output[i] = cont_output[i-1];
+  }
+
+  Serial.println("Errors Array");
+  for(int i = 0; i < num_size; i++){
+    Serial.print(errors[i]);
+    Serial.print(",");
+  }
+
+  Serial.println("cont_array");
+  for(int i = 0; i < denom_size - 1; i++){
+    Serial.print(cont_output[i]);
+    Serial.print(",");
+  }
+
+  float curr_cont_output = 0;
+
+  for(int i = 0; i < num_size; i++){
+    curr_cont_output += numerator[i] * errors[i];
+  }
+  for(int i = 1; i < denom_size; i++){
+    curr_cont_output -= denominator[i] * cont_output[i];
+  }
+
+  curr_cont_output /= denominator[0];
+
+  if(curr_cont_output > 0){
+    setMotorVoltage(curr_cont_output + cw_stiction);
   }
   else{
-    setMotorVoltage(u_z + ccw_stiction);    
+    setMotorVoltage(curr_cont_output + ccw_stiction);    
   }
 
+  cont_output[0] = curr_cont_output;
 
 
-
-
+  delay(23.33375);
   Serial.print(millis());
   Serial.print(","); 
   Serial.print(err);
   Serial.print(","); 
-  Serial.println(motor_pos);
+  Serial.print(motor_pos);
+  Serial.print(","); 
+  
+  Serial.println(curr_cont_output);
 }
 
 // ================== Control ISR ==================
