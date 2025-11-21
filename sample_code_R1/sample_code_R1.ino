17b8f5ae3e09a58c91f0cf2997d5bd9ee7c2747d
n37zhao <n37zhao@uwaterloo.ca>
2025-11-21 13:39:24 -0500

    outer cntrl

--- sample_code_R1/sample_code_R1.ino
+++ sample_code_R1/sample_code_R1.ino
@@ -5,15 +5,20 @@
 int MOT_PIN = A0;   // motor angle sensor
 int BAL_PIN = A1;   // ball position sensor
 
-static constexpr int num_size = 6;
-static constexpr int denom_size = 7;
-double numerator[num_size] = {2.191042856736779 , -1.244715000739053,  0.892620562448263,   0.306902605622103,   0.234368769132498,  -0.197167582568173}; 
-double denominator[denom_size] = {  1.000000000000000,  -0.568099999998879 ,  0.341651128008823 ,  0.028556296287117 ,  0.051161768362048  , 0.029203276104001,   0.008172368274057};
+static constexpr int num_size_outer = 10;
+static constexpr int denom_size_outer = 11;
+static constexpr int num_size_inner = 6;
+static constexpr int denom_size_inner = 7;
+double numerator_inner[num_size_inner] = {2.191042856736779 , -1.244715000739053,  0.892620562448263,   0.306902605622103,   0.234368769132498,  -0.197167582568173}; 
+double denominator_inner[denom_size_inner] = {  1.000000000000000,  -0.568099999998879 ,  0.341651128008823 ,  0.028556296287117 ,  0.051161768362048  , 0.029203276104001,   0.008172368274057};
+double numerator_outer[num_size_outer] = {1, -2.013, -0.3267, 3.473, -2.199, -2.214, 3.83, -0.3601, -2.328, 1.138}; 
+double denominator_outer[denom_size_outer] = {1, -1.782, -0.75, 2.797, -0.4785, -1.442, 0.4939, 0.2962, -0.1165, -0.02184, 0.0057};
 // double numerator[num_size] = {5.221, 2.1935, 3.0126, -1.9452}; 
 // double denominator[denom_size] = {1.0f, 0.4237, 0.4461, 0.2606, 0.0789};
-float errors[num_size] = {0, 0, 0, 0, 0, 0};
-float cont_output[denom_size - 1] = {0, 0, 0, 0, 0, 0};
-
+float errors_inner[num_size_inner] = {0, 0, 0, 0, 0, 0};
+float cont_output_inner[denom_size_inner - 1] = {0, 0, 0, 0, 0, 0};
+float errors_outer[num_size_outer] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+float cont_output_outer[denom_size_outer - 1] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 float voltage;
 
@@ -39,19 +44,22 @@ static constexpr float Kp = 16.0f;
 static constexpr float pi = 3.141592;
 static constexpr float m = - pi/ 228;
 static constexpr float x = 7.03;
-static float target = 0.7;
 static constexpr float cw_stiction = 0.26;
 static constexpr float ccw_stiction = -0.21;
 static int cnter = 0;
+static int outerCnter = 0;
 
 static constexpr float cw_stiction_beam = 0.12;
 static constexpr float ccw_stiction_beam = -0.6;
-
+float target = 0;
 static constexpr float BEAM_LEN = 0.417;
 static constexpr float lever_len = 0.12;
 static constexpr float beam_slope = 0.001064;
 static constexpr float beam_intercept = -0.33088;
 static constexpr float K2 = lever_len / BEAM_LEN;
+static constexpr int inner_sampling = 21;
+static constexpr int outer_sampling = 10 * inner_sampling;
+
 
 void graph_k3(){
   Serial.print(millis());
@@ -77,88 +85,102 @@ void  loop() {
 
 
 
+  
+  float angle_output = 0;
 
-  // float clamped_target = target;
-  // if (clamped_target > 0.7) {
-  //   clamped_target = 0.7;
-  // }
+  if(outerCnter == 15){
+    for(int i = num_size_outer - 1; i > 0; i--){
+      errors_outer[i] = errors_outer[i-1];
+    }
+    float err = target - calc_ball_pos();
+    // Update front of errors array 
+    errors_outer[0] = err;
 
-  // if(cnter >= 128){
-  //   target = -target;
-  //   cnter = 0;
-  // }
-  // cnter++;
+
+    for(int i = denom_size_outer - 2; i > 0; i--){
+      cont_output_outer[i] = cont_output_outer[i-1];
+    }
+
+    for(int i = 0; i < num_size_outer; i++){
+      angle_output += numerator_outer[i] * errors_outer[i];
+    }
+    for(int i = 1; i < denom_size_outer; i++){
+      angle_output -= denominator_outer[i] * cont_output_outer[i];
+    }
+
+    angle_output /= denominator_outer[0];
+    outerCnter = 0;
+  } 
+
+  float clamped_angle = angle_output;
+  if (clamped_angle > 0.7) {
+    clamped_angle = 0.7;
+  }
+  else if(clamped_angle < 0.7){
+    clamped_angle = -0.7;
+  }
 
 
   // // Calculate motor_encoder value, and corresponding motor position
   float motor_val = analogRead(MOT_PIN);
   float motor_pos = m * motor_val + x; 
 
-  // // Calculate Error 
-  // float err = motor_pos - clamped_target;
+  // Calculate Error 
+  float err = motor_pos - clamped_angle;
 
 
-  // // Move errors back 
-  // for(int i = num_size - 1; i > 0; i--){
-  //   errors[i] = errors[i-1];
-  // }
-  // // Update front of errors array 
-  // errors[0] = err;
+  // Move errors back 
+  for(int i = num_size_inner - 1; i > 0; i--){
+    errors_inner[i] = errors_inner[i-1];
+  }
+  // Update front of errors array 
+  errors_inner[0] = err;
 
 
-  // for(int i = denom_size - 2; i > 0; i--){
-  //   cont_output[i] = cont_output[i-1];
-  // }
+  for(int i = denom_size_inner - 2; i > 0; i--){
+    cont_output_inner[i] = cont_output_inner[i-1];
+  }
 
 
-  // float curr_cont_output = 0;
+  float curr_cont_output = 0;
 
-  // for(int i = 0; i < num_size; i++){
-  //   curr_cont_output += numerator[i] * errors[i];
-  // }
-  // for(int i = 1; i < denom_size; i++){
-  //   curr_cont_output -= denominator[i] * cont_output[i];
-  // }
+  for(int i = 0; i < num_size_inner; i++){
+    curr_cont_output += numerator_inner[i] * errors_inner[i];
+  }
+  for(int i = 1; i < denom_size_inner; i++){
+    curr_cont_output -= denominator_inner[i] * cont_output_inner[i];
+  }
 
-  // curr_cont_output /= denominator[0];
+  curr_cont_output /= denominator_inner[0];
 
-  // if(abs(err) <= 0.02){
-  //   setMotorVoltage(0);
-  // }
-  // else{
-  //   if(curr_cont_output > 0){
-  //     setMotorVoltage(curr_cont_output + cw_stiction_beam);
-  //   }
-  //   else{
-  //     setMotorVoltage(curr_cont_output + ccw_stiction_beam);    
-  //   }
-  // }
+  if(abs(err) <= 0.02){
+    setMotorVoltage(0);
+  }
+  else{
+    if(curr_cont_output > 0){
+      setMotorVoltage(curr_cont_output + cw_stiction_beam);
+    }
+    else{
+      setMotorVoltage(curr_cont_output + ccw_stiction_beam);    
+    }
+  }
 
-  // cont_output[0] = curr_cont_output;
+  cont_output_inner[0] = curr_cont_output;
 
-  delay(21);
-  delay(500);
-  
-  voltage -= 0.01;
+  delay(inner_sampling);
+  outerCnter++;
 
-  setMotorVoltage(voltage);
-  // graph_k3();
+
+  float b_pos = calc_ball_pos();
   Serial.print(millis()); 
+  Serial.print(",");
+  Serial.print(err);
+  Serial.print(",");
+  Serial.print(curr_cont_output);
   Serial.print(","); 
   Serial.print(motor_pos);
   Serial.print(",");
-  Serial.println(voltage);
-
-  // float b_pos = calc_ball_pos();
-  // Serial.print(millis()); 
-  // Serial.print(",");
-  // Serial.print(err);
-  // Serial.print(",");
-  // Serial.print(curr_cont_output);
-  // Serial.print(","); 
-  // Serial.print(motor_pos);
-  // Serial.print(",");
-  // Serial.println(b_pos);
+  Serial.println(b_pos);
 
 }
 
